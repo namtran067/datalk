de420baf85545fcac18e793dbca4ed6aba7b36ef
NhatNam067 <nhatnam160701@gmail.com>
2026-06-03 22:54:15 +0700

    dev

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from "nuxt/config"
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
   ssr: false,
 
   modules: [
